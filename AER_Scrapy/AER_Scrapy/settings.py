4c07cce4ea4b74d537733cb73da49a641cb68a79
Georges A <georgesatallahbusiness@gmail.com>
2024-06-06 13:49:31 -0400

    changed settings to evade bot detection

--- AER_Scrapy/AER_Scrapy/settings.py
+++ AER_Scrapy/AER_Scrapy/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "AER_Scrapy.spiders"
 #USER_AGENT = "AER_Scrapy (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -25,7 +25,8 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
+RANDOMIZE_DOWNLOAD_DELAY = True
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
