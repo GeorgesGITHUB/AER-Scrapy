ffebd777e3e9c421c4b399f96453433638caa0fd
Georges A <georgesatallahbusiness@gmail.com>
2024-06-17 16:47:47 -0400

    Fetching PolyID and LandUnit info from csv using Pandas, Account for multiple plotplan downloads, Saved Date from View Attachments, Scrape Cap, Filename fix, Callback renaming

--- AER_Scrapy/AER_Scrapy/spiders/plotplan.py
+++ AER_Scrapy/AER_Scrapy/spiders/plotplan.py
@@ -3,6 +3,17 @@ from scrapy import FormRequest
 from scrapy.http import Request
 from urllib.parse import urljoin
 import os
+import pandas as pd
+
+def csv_to_dict(file_path):
+    # Read the CSV file into a Pandas DataFrame
+    df = pd.read_csv(file_path)
+
+    # Group by 'LandUnit' and aggregate 'PolyID' into lists
+    grouped_data = df.groupby('LandUnit')['PolyID'].apply(list).reset_index()
+
+    # Convert grouped data into a dictionary
+    return dict(zip(grouped_data['LandUnit'], grouped_data['PolyID']))
 
 def createRowsMetaData(htmlRows): # util function
     rowsMeta=[]
@@ -30,18 +41,21 @@ def dprint(*texts):
     print('***')
 
     string = ''
-    for e in texts : string += e + ' '
+    for e in texts : string += str(e) + ' '
     print('****',string)
 
     print('***')
     print('**')
     print('*')
 
+# Paths are relative to where Scrapy crawl is used
 class PlotPlanSpider(scrapy.Spider):
     name = "plotplan"
     url = "https://dds.aer.ca/iar_query/FindApplications.aspx"
-    userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
-
+    userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+    LandUnit_PolyID_Dict = csv_to_dict('AB_2023_Polygon_with_Source_LandUnit.csv')
+    number_of_scrapes_allowed= 1 # set to -1 to disable limit
+    
     def start_requests(self):
         dprint('AER_Scrapy by Georges Atallah')
         dprint('Spider', self.name, 'making a Request to', self.url)
@@ -53,34 +67,48 @@ class PlotPlanSpider(scrapy.Spider):
         yield scrapy.Request(
             url=self.url, 
             headers=headers, 
-            callback=self.step1
+            callback=self.step1,
         )
 
     # Submitting a query
     def step1(self, response):
         dprint('Starting step1')
 
-        formData = {
-            'LSD': '13',
-            'Section': '24',
-            'Township': '50',
-            'Range': '09',
-            'Meridian': '4',
-            'btnSearch': 'Search',
-            '_EubIapPageUseProgressMonitor': 'true',
-        }
+        for index, key in enumerate(self.LandUnit_PolyID_Dict):
+            if index==self.number_of_scrapes_allowed:
+                dprint('Reached the limit of permitted LandUnit scrapes:', self.number_of_scrapes_allowed)
+                break
 
-        headers = {
-            'User-Agent': self.userAgent,
-        }
+            landUnit = key.split('-')
+            temp = landUnit[3].split('W')
+            landUnit[3]=temp[0]
+            landUnit.append(temp[1])
+            temp=None
+            
+            formData = {
+                'LSD': landUnit[0],
+                'Section': landUnit[1],
+                'Township': landUnit[2],
+                'Range': landUnit[3],
+                'Meridian': landUnit[4],
+                'btnSearch': 'Search',
+                '_EubIapPageUseProgressMonitor': 'true',
+            }
+            
+            print('Form Data Entered:', formData)
 
-        # from_response handles aspx double underscore hidden fields
-        yield FormRequest.from_response(
-            response=response, 
-            formdata=formData, 
-            headers=headers, 
-            callback=self.step2
-        )
+            headers = {
+                'User-Agent': self.userAgent,
+            }
+
+            # from_response handles aspx double underscore hidden fields
+            yield FormRequest.from_response(
+                response=response, 
+                formdata=formData, 
+                headers=headers, 
+                callback=self.step2,
+                meta={'LandUnit':key}
+            )
     
     # Reloads Query results to display 100 items
     def step2(self, response):
@@ -100,7 +128,8 @@ class PlotPlanSpider(scrapy.Spider):
             response=response, 
             formdata=formData, 
             headers=headers, 
-            callback=self.step3
+            callback=self.step3,
+            meta=response.meta # passing meta forward
         )
 
     # Gets row data of targetted rows then views them
@@ -113,6 +142,7 @@ class PlotPlanSpider(scrapy.Spider):
         rows = rows.xpath('td[1][input[@value="View"]]/..')
 
         # Saving meta data of rows
+        # Row[ View | App# | Alt# | Status | Primary Applicant | Registered | Category | Type | Location ]
         rowsMeta = createRowsMetaData(rows)
         
         headers = {
@@ -125,12 +155,14 @@ class PlotPlanSpider(scrapy.Spider):
                 rowMeta['View_name']:'View',
                 '_EubIapPageUseProgressMonitor':'true',
             }
+
+            response.meta['App#']=rowMeta['App#']
             yield FormRequest.from_response(
                 response=response,
                 formdata=formData,
                 headers=headers,
                 callback=self.step4,
-                meta=rowMeta,
+                meta=response.meta,
             )
 
     # Visit the View Attachments page
@@ -154,30 +186,40 @@ class PlotPlanSpider(scrapy.Spider):
     # Initiates a download requests for plotplan attachments
     def step5(self, response):
         dprint('Starting step5 for Application Number', response.meta['App#'])
-        
-        # Extract the href value of the anchor tag with the text "Plot Plan"
-        relative_url = response.xpath('//a[text()="Plot Plan"]/@href').get()
 
-        file_url = urljoin(response.url, relative_url)
+        # Extract all rows where the 2nd column contains an anchor tag containing "Plot Plan"
+        rows = response.xpath('//table//tr[td[2]//a[contains(text(), "Plot Plan")]]')
+        if len(rows) == 0: dprint('No plot plan found in View Attachments')
 
-        headers = {
-            'User-Agent': self.userAgent,
-        }
+        for row in rows:
+            # Get the relative URL from the 2nd column
+            relative_url = row.xpath('td[2]//a/@href').get()
+            
+            # Get the date from the 4th column
+            date = row.xpath('td[4]/text()').get()
 
-        yield Request(
-            url=file_url,
-            headers= headers,
-            callback=self.step6,
-            # meta=response.meta,
-        )
+            file_url = urljoin(response.url, relative_url)
+
+            headers = {
+                'User-Agent': self.userAgent,
+            }
+
+            response.meta['date']=date
+            response.meta['PolyID']= self.LandUnit_PolyID_Dict[response.meta['LandUnit']][0]
+            print('final response meta', response.meta)
+            yield Request(
+                url=file_url,
+                headers= headers,
+                callback=self.step6,
+                meta=response.meta,
+            )
 
     # Downloads plotplans into a directory
     def step6(self, response):
         dprint('Starting step6')
 
-        # Using query part as filename and adding .pdf extension
-        original_filename = response.url.split('?')[-1] + '.pdf'
-        modified_filename = f"modified_{original_filename}"
+        # PolyID + Date in View Attachments + Doc Number
+        filename = f"{response.meta['PolyID']}___{response.meta['date']}_{response.url.split('?DOCNUM=')[-1]}.pdf"
         
         # Create the directory if it does not exist
         directory = 'scraped-data'
@@ -185,10 +227,10 @@ class PlotPlanSpider(scrapy.Spider):
             os.makedirs(directory)
         
         # Handle filename conflicts
-        file_path = os.path.join(directory, modified_filename)
+        file_path = os.path.join(directory, filename)
         counter = 1
         while os.path.exists(file_path):
-            file_path = os.path.join(directory, f"modified_{counter}_{original_filename}")
+            file_path = os.path.join(directory, f"{filename[:-4]}_({counter}).pdf")
             counter += 1
         
         # Save the file
